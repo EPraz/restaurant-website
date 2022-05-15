56f101bd81c116b85bd165a6557cfe7a6defa00d
Edwin Pile <epilepra@gmail.com>
2022-05-15 18:03:15 -0500

    footer added

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -10,7 +10,7 @@ const FindUs = () => (
         <p className='p__opensans'>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 75G</p>
         <p className='p__cormorant' style={{color: '#dcca87', margin: '2rem 0'}}>Opening Hours</p>
         <p className='p__opensans'>Mon - Fri: 10:00  am - 02:00 Am</p>
-        <p classNAme='p__opensans'>Sat - Sun: 10:00  am - 03:00 Am</p>
+        <p className='p__opensans'>Sat - Sun: 10:00  am - 03:00 Am</p>
       </div>
       <button type='button' className='custom__button' style={{marginTop: '2rem'}}>Visit Us</button>
     </div>
